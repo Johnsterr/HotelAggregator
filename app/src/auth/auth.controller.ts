69a0d6819e3c282c8eddf1ac707931b81304ed3f
Evgeniy <evgpashko92@gmail.com>
2023-03-03 23:34:29 +0700

    local auth guard

--- app/src/auth/auth.controller.ts
+++ app/src/auth/auth.controller.ts
@@ -8,14 +8,14 @@ import {
   HttpStatus,
   UseGuards,
 } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from "./auth.service";
+import { LocalAuthGuard } from "./guards/local-auth.guard";
 
 @Controller("api")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(AuthGuard("local"))
+  @UseGuards(LocalAuthGuard)
   @Post("/auth/login")
   @UsePipes(
     new ValidationPipe({
