7fe4f8e8b60931bf49c18383b37cab24b7f05304
Evgeniy <evgpashko92@gmail.com>
2023-03-10 19:35:34 +0700

    base search params

--- app/src/types/general.ts
+++ app/src/types/general.ts
@@ -1,3 +1,17 @@
 import { ObjectId } from "mongoose";
+import { Type } from "class-transformer";
+import { IsNumber, IsOptional } from "class-validator";
 
 export type ID = string | ObjectId;
+
+export class SearchBaseParams {
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
+  limit?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
+  offset?: number;
+}
